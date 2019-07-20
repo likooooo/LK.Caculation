5697b506d3e8fe49e57375dc90a56dc1f8fbc350
like <462866046@qq.com>
2019-07-20 10:56:30 +0800

    第一次提交

---
+++ Sample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using LK.Caculation;
+namespace Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] gg = new float[3] { 1f, 2f, 3f };
+          var res=  Caculation.Sum(gg);
+        }
+    }
+}
---
+++ 常用数据处理/Caculation.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+//using System.Data.Objects;//Assembly:System.Data.Entity.dll
+
+namespace LK.Caculation
+{
+    //参考
+    //https://docs.microsoft.com/zh-cn/dotnet/api/system.linq.enumerable?f1url=https%3A%2F%2Fmsdn.microsoft.com%2Fquery%2Fdev15.query%3FappId%3DDev15IDEF1%26l%3DZH-CN%26k%3Dk(System.Linq.Enumerable);k(TargetFrameworkMoniker-.NETFramework,Version%3Dv4.0);k(DevLang-csharp)%26rd%3Dtrue&view=netframework-4.8
+
+
+    public static class Caculation
+    {
+        //数据最大值
+        public static float Max(IEnumerable<float> arry)
+            => (arry.Max());
+
+
+        //数据最小值
+        public static float Min(IEnumerable<float> arry)
+            => (arry.Min());
+
+
+        //数据长度
+        public static float Length(IEnumerable<float> arry)
+            => (arry.LongCount());
+
+       
+        //两个数组合并
+        public static IEnumerable<float> Union(IEnumerable<float> arry,IEnumerable<float> second)
+            => (arry.Union(second));
+
+
+        //求和
+        public static float Sum(IEnumerable<float> arry)
+            => (arry.Sum());
+
+
+        //求均值
+        public static float Mean(IEnumerable<float> arry)
+         => (arry.Average()); //   => (Sum(arry) / Length(arry));
+
+
+        //方差
+        public static float Variance(IEnumerable<float> arry)
+        {
+            float mean = Mean(arry);
+            return Mean(arry.Select(s => (s - mean) * (s - mean)));
+        }
+
+
+        //标准差(存在问题)
+
+        public static float Deviation(IEnumerable<float> arry)
+        => (float)Math.Sqrt((double)Variance(arry));
+    }
+}
